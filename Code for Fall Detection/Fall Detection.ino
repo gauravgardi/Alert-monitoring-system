ee35b68be91df3e7d57b37cee4257cb1b7f6d9e2
Gaurav Gardi <gaurav.gardi2@gmail.com>
2017-01-06 20:47:41 +0530

    Update Fall Detection.ino

--- Code for Fall Detection/Fall Detection.ino
+++ Code for Fall Detection/Fall Detection.ino
@@ -1,9 +1,3 @@
-// MPU-6050 Short Example Sketch
-// By Arduino User JohnChi
-// August 17, 2014
-// Public Domain
-
-
 ////////////////////////////headers///////////////////////////////////////////////////////////
 #include<Wire.h>
 #include<SoftwareSerial.h>
